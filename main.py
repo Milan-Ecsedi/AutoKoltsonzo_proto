1083d4a6f1a7de7a0c0638e665857879a3510bd5
Ecsedi Milan <milan200314@gmail.com>
2025-06-19 09:35:58 +0200

    clear ups

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ autoKolcsonzo1.auto_hozzaad(auto1)
 autoKolcsonzo1.auto_hozzaad(auto2)
 autoKolcsonzo1.auto_hozzaad(auto3)
 
-
 autoKolcsonzo1.berel_auto(auto1, datetime.date(2025, 12,11))
 autoKolcsonzo1.berel_auto(auto2, datetime.date(2025, 11, 14))
 autoKolcsonzo1.berel_auto(auto3, datetime.date(2025, 9, 10))
@@ -36,7 +35,6 @@ while True:
         i = int(input("Az elérhető bérelhető járművekből adja meg a sorszámát amit ki szeretne választani: "))
         kivalasztottAuto= autoKolcsonzo1.get_auto_as_object()[i-1]
         print("A kiválasztott gépjármű: "+ str(kivalasztottAuto))
-        
         datum_str= input("Adja meg a bérlés dátumát (pl: 2025-12-07 ): ")
         datum = datetime.datetime.strptime(datum_str, "%Y-%m-%d").date()
         #print(type(datum))
@@ -46,7 +44,6 @@ while True:
         i = int(input("Az elérhető bérelhető járművekből adja meg a sorszámát amit le szeretne mondani: "))
         kivalasztottAuto= autoKolcsonzo1.get_auto_as_object()[i-1]
         print("A kiválasztott gépjármű: "+ str(kivalasztottAuto))
-        
         datum_str= input("Adja meg azt a napot mikor lefoglalta (pl: 2025-12-07 ) : ")
         datum = datetime.datetime.strptime(datum_str, "%Y-%m-%d").date()
         #print(type(datum))
